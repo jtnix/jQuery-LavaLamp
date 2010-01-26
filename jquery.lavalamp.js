028baff0e2e912aabba23287341b858f49cc8c29
Jolyon Terwilliger <jolyon@nixbox.com>
2010-01-26 14:10:28 -0500

    v1.3.4a commit

--- jquery.lavalamp.js
+++ jquery.lavalamp.js
@@ -1,134 +1,135 @@
 /**
- * jquery.LavaLamp enhances an unordered list of menu-items with a ballooning animated effect
- * similar to the Lava Lamps of the 1970s. Use the CSS provided with the demos at
- * http://nixboxdesigns.com/demos/jquery-lavalamp-demos.html or experiment with your own styles 
- *
- * Requires jQuery v1.3.2
- *
- * http://nixboxdesigns.com/demos/jquery-lavalamp.php
- *
- * Copyright (c) 2008, 2009 Jolyon Terwilliger, jolyon@nixbox.com
- * Source code Copyright (c) 2007, 2008
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- * credits to Guillermo Rauch and Ganeshji Marwaha (gmarwaha.com) for previous editions
- *
- * Version: 1.0 - adapted for jQuery 1.2.x series
- * Version: 1.1 - added linum parameter
- * Version: 1.2 - modified to support vertical resizing of elements
- * Version: 1.3 - enhanced automatic <li> item hi-lighting - will attempt to
- *					lock onto li > a element with href closest to selected
- *					window.location
- *			 	- click always returns 'true' by default, for standard link follow through.
- *
- * Version: 1.3.1 - verified for use with jQuery 1.3 - should still work with 1.2.x series
- *				- changed linum parameter to startItem for clarity
- *				- improved slide-in accuracy for .back elements with borders
- *				- changed .current class to .selectedLava for clarity and improved
- *					support
- *				- appended 'Lava' to all internal class names to avoid conflicts
- *				- fixed bug applying selectedLava class to elements with matching
- *					location.hash
- *				- now supports jquery.compat plugin for cross-library support
- *				- performance improvements
- *				- added new options:
- *				autoReturn: true - if set to false, hover will not return to last selected
- *									item upon list mouseout.
- *				returnDelay: 0 - if set, will delay auto-return feature specified # of
- *									milliseconds.
- *				setOnClick: true - if set to false, hover will return to default element
- *									regardless of click event.
- *				homeLeft: 0, homeTop: 0 - if either set to non zero value, absolute
- *									positioned li element with class .homeLava is 
- *									prepended to list for homing feature.
- *				homeWidth: 0, homeHeight: 0 - if set, are used for creation of li.homeLava
- *									element.
- *				returnHome: false - if set along with homeLeft or homeTop, lavalamp hover
- *									will always return to li.home after click.
- *
- * Version: 1.3.2 - fixed: stray $ references inside the plugin to work with
- * 					jQuery.noConflict() properly - thanks Colin.
- *
- * Version: 1.3.3 - fixed: added closure with null passed argument for move() command in
- * 					returnDelay to fix errors some were seeing - thanks to Michel and 
- *					Richard for noticing this.
- *
- *					fixed: changed mouseover/out events to mouseenter/leave to fix jerky
- *					animation problem when using excessive margins instead of padding.  
- *					Thanks to Thomas for the solution and Chris for demonstrating the problem.
- *					this fix requires jQuery 1.3.2 to work.
- *
- *					enhanced: added 'noLava' class detection to prevent LavaLamp effect
- *					application to LI elements with this class. This feature allows you to
- *					create submenus - for details, see examples at
- *					http://nixboxdesigns.com/demos/jquery-lavalamp-demos.html
- *
- *					enhanced: modified to better automatically find default location for 
- *					relative links. Thanks to Harold for testing and finding this bug.
- *
- * Examples and usage:
- *
- * The HTML markup used to build the menu can be as simple as...
- *
- *       <ul class="lavaLamp">
- *           <li><a href="#">Phone Home</a></li>
- *           <li><a href="#">Make Contact</a></li>
- *           <li><a href="#">Board Ship</a></li>
- *           <li><a href="#">Fly to Venus</a></li>
- *       </ul>
- *
- * Once you have included the style sheet that comes with the plugin, you will have to include 
- * a reference to the jQuery library, easing plugin (optional) and the LavaLamp(this) plugin.
- *
- * Use the following snippet to initialize the menu using jQuery easing library::
- * Easing Library 1.3 available here:  http://plugins.jquery.com/project/Easing
- * 
- *   $(function() { $(".lavaLamp").lavaLamp({ fx: "easeOutBack", speed: 700}) });
- *
- * @param Object - You can specify all the options shown below as object variables:
- *
- * @option fx - default is "swing"
- * @example
- * $(".lavaLamp").lavaLamp({ fx: "easeOutElastic" });
- * @desc Creates a menu with "Elastic" easing effect. You need to include the easing plugin for this to work.
- *
- * @option speed - default is 500 ms
- * @example
- * $(".lavaLamp").lavaLamp({ speed: 500 });
- * @desc Creates a menu with an animation speed of 500 ms.
- *
- * @option click - no defaults
- * @example
- * $(".lavaLamp").lavaLamp({ click: function(event, menuItem) { return false; } });
- * @desc You can supply a callback to be executed when the menu item is clicked. 
- * The event object and the menu-item that was clicked will be passed in as arguments.
- * 
- * @option startItem - default is 'no'
- * @example
- * jQuery(".lavaLamp").lavaLamp({ startItem: 2 });
- * @desc startItem specifies the li element to default to, beginning with 0 for the first li element 
- * within the parent UL or OL used to initialize lavaLamp.  This can be used to set default
- * lavaLamp hilight on page reloads.
+ jQuery LavaLamp 1.3.4a 
+
+ Requires jQuery v1.3.2 or better from http://jquery.com
+
+ http://nixboxdesigns.com/projects/jquery-lavalamp/
+
+ Copyright (c) 2008, 2009 Jolyon Terwilliger, jolyon@nixbox.com
+ Source code Copyright (c) 2008, 2009
+ Dual licensed under the MIT and GPL licenses:
+ http://www.opensource.org/licenses/mit-license.php
+ http://www.gnu.org/licenses/gpl.html
+
+ credits to Guillermo Rauch and Ganeshji Marwaha (gmarwaha.com) for previous editions
+
+ Version: 1.0 - adapted for jQuery 1.2.x series
+ Version: 1.1 - added linum parameter
+ Version: 1.2 - modified to support vertical resizing of elements
+ Version: 1.3 - enhanced automatic <li> item hi-lighting - will attempt to
+					lock onto li > a element with href closest to selected
+					window.location
+			 	- click always returns 'true' by default, for standard link follow through.
+
+ Version: 1.3.1 - verified for use with jQuery 1.3 - should still work with 1.2.x series
+				- changed linum parameter to startItem for clarity
+				- improved slide-in accuracy for .back elements with borders
+				- changed .current class to .selectedLava for clarity and improved
+					support
+				- appended 'Lava' to all internal class names to avoid conflicts
+				- fixed bug applying selectedLava class to elements with matching
+					location.hash
+				- now supports jquery.compat plugin for cross-library support
+				- performance improvements
+				- added new options:
+				autoReturn: true - if set to false, hover will not return to last selected
+									item upon list mouseout.
+				returnDelay: 0 - if set, will delay auto-return feature specified # of
+									milliseconds.
+				setOnClick: true - if set to false, hover will return to default element
+									regardless of click event.
+				homeLeft: 0, homeTop: 0 - if either set to non zero value, absolute
+									positioned li element with class .homeLava is 
+									prepended to list for homing feature.
+				homeWidth: 0, homeHeight: 0 - if set, are used for creation of li.homeLava
+									element.
+				returnHome: false - if set along with homeLeft or homeTop, lavalamp hover
+									will always return to li.home after click.
+
+ Version: 1.3.2 - fixed: stray $ references inside the plugin to work with
+ 					jQuery.noConflict() properly - thanks Colin.
+
+ Version: 1.3.3 - fixed: added closure with null passed argument for move() command in
+ 					returnDelay to fix errors some were seeing - thanks to Michel and 
+					Richard for noticing this.
+
+					fixed: changed mouseover/out events to mouseenter/leave to fix jerky
+					animation problem when using excessive margins instead of padding.  
+					Thanks to Thomas for the solution and Chris for demonstrating the problem.
+
+					enhanced: added 'noLava' class detection to prevent LavaLamp effect
+					application to LI elements with this class. This feature allows you to
+					create submenus - for details, see examples at
+					http://nixboxdesigns.com/demos/jquery-lavalamp-demos.html
+
+					enhanced: modified to better automatically find default location for 
+					relative links. Thanks to Harold for testing and finding this bug.
+
+ Examples and usage:
+
+ The HTML markup used to build the menu can be as simple as...
+
+       <ul class="lavaLamp">
+           <li><a href="#">Phone Home</a></li>
+           <li><a href="#">Make Contact</a></li>
+           <li><a href="#">Board Ship</a></li>
+           <li><a href="#">Fly to Venus</a></li>
+       </ul>
+
+ Once you have included the style sheet that comes with the plugin, you will have to include 
+ a reference to the jQuery library, easing plugin (optional) and the LavaLamp(this) plugin.
+
+ Use the following snippet to initialize the menu using jQuery easing library::
+ Easing Library 1.3 available here:  http://plugins.jquery.com/project/Easing
+ 
+ Example:
+ $(function() { $(".lavaLamp").lavaLamp({ fx: "easeOutBack", speed: 700}) });
+ sets the easing to 'Out, Back' with a speed of 7/10ths of a second.
+
+ @param fx - default: 'swing'
+ Creates a menu with "Elastic" easing effect. You need to include the easing plugin for the example to work.
+ 
+ Example:
+ $(".lavaLamp").lavaLamp({ fx: "easeOutElastic" });
+ 
+ @param speed - default: 500
+ sets animation speed in milliseconds
+ 
+ Example:
+ $(".lavaLamp").lavaLamp({ speed: 1000 });
+ sets the animation speed to one second.
+ 
+ @param onclick - default: function() { return true; }
+ Callback to be executed when the menu item is clicked. The 'event' object and the menu-item that was clicked will be passed in as arguments.
+ 
+ Example:
+ $(".lavaLamp").lavaLamp({ click: function(event, menuItem) { return false; } });
+
+ @param startItem - default: 'no'
+ specifies the number li element to default hover on, starting with 0 for the first li element within the parent UL or OL list used to initialize lavaLamp.  Used to manually set the default lavaLamp hi-light on load.
+
+ Example:
+ jQuery(".lavaLamp").lavaLamp({ startItem: 2 });
+ selects the third element in the list
  */
 
 (function(jQuery) {
 jQuery.fn.lavaLamp = function(o) {
+
 	o = jQuery.extend({ fx: 'swing',
-					  	speed: 500, 
-						click: function(){return true}, 
-						startItem: 'no',
-						autoReturn: true,
-						returnDelay: 0,
-						setOnClick: true,
-						homeTop:0,
-						homeLeft:0,
-						homeWidth:0,
-						homeHeight:0,
-						returnHome:false
-						}, 
-					o || {});
+				speed: 500, 
+				click: function(){return true}, 
+				startItem: 'no',
+				autoReturn: true,
+				returnDelay: 0,
+				setOnClick: true,
+				homeTop:0,
+				homeLeft:0,
+				homeWidth:0,
+				homeHeight:0,
+				returnHome:false
+				}, 
+			o || {});
 
 	var $home;
 	// create homeLava element if origin dimensions set
@@ -138,13 +139,8 @@ jQuery.fn.lavaLamp = function(o) {
 	}
 		
 	return this.each(function() {
-		var path = location.pathname + location.search + location.hash;
-		var $selected = new Object;
-		var delayTimer;
-		var $back;
-		var ce; //current_element
-		
-		var $li = jQuery('li[class!=noLava]', this);
+		var path = location.pathname + location.search + location.hash, $selected, $back, $li = jQuery('li[class!=noLava]', this), delayTimer, ce // current element
+
 		// check for complete path match, if so flag element into $selected
 		if ( o.startItem == 'no' )
 			$selected = jQuery('li a[href$="' + path + '"]', this).parent('li');
@@ -209,10 +205,12 @@ jQuery.fn.lavaLamp = function(o) {
 			if (!el) el = ce;
 			// .backLava element border check and animation fix
 			var bx=0, by=0;
-			if (!jQuery.browser.msie) {
+			/* rethink
+			if (jQuery.browser.msie) {
 				bx = ($back.outerWidth() - $back.innerWidth())/2;
 				by = ($back.outerHeight() - $back.innerHeight())/2;
 			}
+			*/
 			$back.stop()
 			.animate({
 				left: el.offsetLeft-bx,
